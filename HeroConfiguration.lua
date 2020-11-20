ad8326bd38317633d640aa1ce9a7e97b74cbae01
Raphaël Bonnet <harcyah@gmail.com>
2020-11-20 09:13:45 +0100

    Use single quotes around strings

--- HeroConfiguration.lua
+++ HeroConfiguration.lua
@@ -1,17 +1,17 @@
 
-local frame = CreateFrame("Frame");
-frame:RegisterEvent("ADDON_LOADED");
-frame:RegisterEvent("PLAYER_ENTERING_WORLD");
+local frame = CreateFrame('Frame');
+frame:RegisterEvent('ADDON_LOADED');
+frame:RegisterEvent('PLAYER_ENTERING_WORLD');
 frame:Hide();
 
-local TRACKING_LOW_LEVEL_QUESTS = "Quêtes de bas niveau"
-local TRACKING_FLY_MASTER = "Maître de vol"
-local TRACKING_ARCHAEOLOGY_SITES = "Suivi des sites de fouille"
-local TRACKING_MINE_SPOTS = "Découverte de gisements"
-local TRACKING_HERB_SPOTS = "Découverte d\'herbes"
-local TRACKING_FISH_SPOTS = "Découverte de poissons"
-local TRACKING_TREASURE_SPOTS = "Découverte de trésors"
-local TRACKING_QUESTS_SPOTS = "Suivi des points d’intérêts de quêtes"
+local TRACKING_LOW_LEVEL_QUESTS = 'Quêtes de bas niveau'
+local TRACKING_FLY_MASTER = 'Maître de vol'
+local TRACKING_ARCHAEOLOGY_SITES = 'Suivi des sites de fouille'
+local TRACKING_MINE_SPOTS = 'Découverte de gisements'
+local TRACKING_HERB_SPOTS = 'Découverte d\'herbes'
+local TRACKING_FISH_SPOTS = 'Découverte de poissons'
+local TRACKING_TREASURE_SPOTS = 'Découverte de trésors'
+local TRACKING_QUESTS_SPOTS = 'Suivi des points d’intérêts de quêtes'
 
 local DEBUG = false;
 
@@ -23,7 +23,7 @@ end
 
 local function OnTrackingInfo(i, value, name)
 	SetTracking(i, value);
-	Log("Defined tracking " .. name .. " : " .. tostring(value));
+	Log('Defined tracking ' .. name .. ' : ' .. tostring(value));
 end
 
 local function ConfigureTracking()
@@ -48,58 +48,58 @@ local function ConfigureTracking()
 			OnTrackingInfo(i, HeroConfiguration.trackQuestsSpots, TRACKING_QUESTS_SPOTS);
 		else
 			SetTracking(i, false);
-			Log("Defined tracking " .. name .. " : false (default)");
+			Log('Defined tracking ' .. name .. ' : false (default)');
 		end
 	end
 end
 
 local function ConfigureScreenshots()
-	SetCVar("screenshotFormat", "tga");
-	SetCVar("screenshotQuality", 10);
+	SetCVar('screenshotFormat', 'tga');
+	SetCVar('screenshotQuality', 10);
 end
 
 local function ConfigureUnitNames()
-	SetCVar("UnitNameNPC", 1);
-	SetCVar("UnitNameOwn", 0);
-	SetCVar("UnitNamePlayerGuild", 0);
-	SetCVar("UnitNamePlayerPVPTitle", 0);
-	SetCVar("UnitNameEnemyGuardianName", 1);
-	SetCVar("UnitNameEnemyPetName", 0);
-	SetCVar("UnitNameEnemyMinionName", 0);
-	SetCVar("UnitNameEnemyPlayerName", 1);
-	SetCVar("UnitNameEnemyTotemName", 0);
-	SetCVar("UnitNameFriendlyGuardianName", 1);
-	SetCVar("UnitNameFriendlyPetName", 0);
-	SetCVar("UnitNameFriendlyMinionName", 0);
-	SetCVar("UnitNameFriendlyPlayerName", 1);
-	SetCVar("UnitNameFriendlyTotemName", 0);
+	SetCVar('UnitNameNPC', 1);
+	SetCVar('UnitNameOwn', 0);
+	SetCVar('UnitNamePlayerGuild', 0);
+	SetCVar('UnitNamePlayerPVPTitle', 0);
+	SetCVar('UnitNameEnemyGuardianName', 1);
+	SetCVar('UnitNameEnemyPetName', 0);
+	SetCVar('UnitNameEnemyMinionName', 0);
+	SetCVar('UnitNameEnemyPlayerName', 1);
+	SetCVar('UnitNameEnemyTotemName', 0);
+	SetCVar('UnitNameFriendlyGuardianName', 1);
+	SetCVar('UnitNameFriendlyPetName', 0);
+	SetCVar('UnitNameFriendlyMinionName', 0);
+	SetCVar('UnitNameFriendlyPlayerName', 1);
+	SetCVar('UnitNameFriendlyTotemName', 0);
 end
 
 local function ConfigureNameplates()
-	SetCVar("nameplateShowSelf", 1);
-	SetCVar("NameplatePersonalShowAlways", 0);
-	SetCVar("NameplatePersonalShowInCombat", 1);
-	SetCVar("NameplatePersonalShowWithTarget", 1);
+	SetCVar('nameplateShowSelf', 1);
+	SetCVar('NameplatePersonalShowAlways', 0);
+	SetCVar('NameplatePersonalShowInCombat', 1);
+	SetCVar('NameplatePersonalShowWithTarget', 1);
 end
 
 local function ConfigureMisc()
-	SetCVar("autoLootDefault", 1);
-	SetCVar("buffDurations", 1);
-	SetCVar("consolidateBuffs", 0);
-	SetCVar("chatBubbles", 0);
-	SetCVar("chatBubblesParty", 0);
-	SetCVar("chatMouseScroll", 1);
-	SetCVar("removeChatDelay", 1);
-	SetCVar("instantQuestText", 1);
-	SetCVar("showTutorials", 0);
-	SetCVar("violenceLevel", 5);
-	SetCVar("showDungeonEntrancesOnMap", 1);
+	SetCVar('autoLootDefault', 1);
+	SetCVar('buffDurations', 1);
+	SetCVar('consolidateBuffs', 0);
+	SetCVar('chatBubbles', 0);
+	SetCVar('chatBubblesParty', 0);
+	SetCVar('chatMouseScroll', 1);
+	SetCVar('removeChatDelay', 1);
+	SetCVar('instantQuestText', 1);
+	SetCVar('showTutorials', 0);
+	SetCVar('violenceLevel', 5);
+	SetCVar('showDungeonEntrancesOnMap', 1);
 end
 
-frame:SetScript("OnEvent", function(self, event, ...)
+frame:SetScript('OnEvent', function(self, event, ...)
 	local arg = {...}
 
-	if (event == "ADDON_LOADED" and arg[1] == 'HeroConfiguration') then
+	if (event == 'ADDON_LOADED' and arg[1] == 'HeroConfiguration') then
 		if (HeroConfiguration == nil) then
 			HeroConfiguration = {};
 		end
@@ -129,7 +129,7 @@ frame:SetScript("OnEvent", function(self, event, ...)
 		end
 	end
 
-	if (event == "PLAYER_ENTERING_WORLD") then
+	if (event == 'PLAYER_ENTERING_WORLD') then
 		ConfigureTracking();
 		ConfigureScreenshots();
 		ConfigureUnitNames();
